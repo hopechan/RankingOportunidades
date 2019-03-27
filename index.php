e81d837b600faf7de7fd9986735beffcabdb61e0
Esperanza Dueñas <da13002@ues.edu.sv>
2019-03-27 11:02:28 -0600

    Vista principal

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RANKING OPORTUNIDADES</title>
+</head>
+<body>
+    <h2>RANKING OPORTUNIDADES</h2>
+    <ul>
+        <a href="index.php">Inicio</a>
+        <a href="./vista/verTipos.php">Ver Tipos</a>
+        <a href="./vista/webCertificacion.php">Certificaciones</a>
+    </ul>
+</body>
+</html>
