c205133ed00189530259ad173716c2e40fcc073b
Esperanza Dueñas <da13002@ues.edu.sv>
2019-03-28 22:54:14 -0600

    index actualizado

--- index.php
+++ index.php
@@ -4,14 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./vista/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>RANKING OPORTUNIDADES</title>
 </head>
 <body>
-    <h2>RANKING OPORTUNIDADES</h2>
-    <ul>
-        <a href="index.php">Inicio</a>
-        <a href="./vista/verTipos.php">Ver Tipos</a>
-        <a href="./vista/webCertificacion.php">Certificaciones</a>
-    </ul>
+    <section class="barra">
+        <a href=""><img src="./vista/img/logo.png" alt="logo Oportunidades"></a>
+    </section>
+    <section class="lateral">
+        <a href="index.php"><i class="fas fa-home"></i> Inicio</a>
+        <a href="./vista/webEstudiantes.php"><i class="fas fa-user-graduate"></i> Estudiantes</a>
+        <a href="./vista/webNotas.php"><i class="fas fa-book-open"></i> Notas</a>
+        <a href="./vista/webCertificacion.php"><i class="fas fa-certificate"></i> Certificaciones</a>
+    </section>
+    <!--
+    <section class="main">
+        <h2>Ranking</h2>
+        <table>
+            <tr>
+                <th>Alumno</th>
+                <th>Nota</th>
+                <th>Opciones</th>
+            </tr>
+            <tr>
+                <td>Esperanza Elizabeth Dueñas Acostas</td>
+                <td>9.50</td>
+                <td><a href="#">Detalle</a></td>
+            </tr>
+            <tr>
+                <td>Roberto Antonio Morales</td>
+                <td>9.00</td>
+                <td><a href="#">Detalle</a></td>
+            </tr>
+        </table>
+        <br><br>
+    </section>
+    -->
 </body>
 </html>
