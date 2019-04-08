aba3b2d8fcc4730daafddac4ae4f69a095bfc83a
robertomorales21 <48929087+robertomorales21@users.noreply.github.com>
2019-04-08 14:21:56 -0600

    Ya funciona el agregar Nota UUUUUUUUUUU

--- vista/agregarNota.php
+++ vista/agregarNota.php
@@ -5,27 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Agregar Notas</title>
-    <!--<link rel="stylesheet" href="./css/style.css">-->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <article>
+    <article>   
         <table width="1000px">
             <tr>
                 <th colspan="4">
-                    Agregar Nota
+                    Agregar Nota 
                 </th>
             </tr>
-            <tr>
-                <td colspan="2">
                 <form method="post" class="estilo">
-                    Seleccione el numero de evaluaciones:
-                </td> 
-                <td>
-                    <input type="number" name="Evaluaciones" min="1" max="4"> &nbsp;
-                    <input type="submit" value="Generar" name="gen">
-                </td>
-            </tr>
-            <tr>
                 <td colspan="2">
                     Elija un tipo de evaluación:  <br>
                     <select name="cmbTipo" class="estilo">
@@ -65,7 +55,7 @@
                             $cm = new controladorMate();
                             $materia = $cm->ObtenerMateria();
                             for ($mat=0; $mat < sizeof($materia) ; $mat++) { 
-                                echo "<option value='" . $materia[$mat]->getIdMateria() . "'>" . $materia[$mat]->getMateria() . "</option>";
+                               echo "<option value='" . $materia[$mat]->getIdMateria() . "'>" . $materia[$mat]->getMateria() . "</option>";
                             }
                         ?>
                     </select>
@@ -80,74 +70,87 @@
                     </select>
                 </td>
             </tr>
-            <tr>
-                <td colspan="4">
-                    <?php
-                        if (isset($_POST['gen'])) {
-                            $evaluaciones = $_POST['Evaluaciones'];
-                        for ($a=0; $a < $evaluaciones; $a++) { 
-                            echo "Nota " . ($a+1) . "&nbsp;&nbsp;";
-                            echo "<input type='number' name='notas[]' min='1' max='10'>&nbsp;&nbsp;";
-                        }
-                        echo "<input type='submit' value='Ok' name='save'><br><br>";
-                        echo " <input type='hidden' name='valores' value='" . $evaluaciones . "'>";
-                        }
-                    ?>
+        <?php
+            echo "<tr>";
+               echo ' <td colspan="4">
+                    Nota 1 &nbsp;&nbsp;
+                    <input type="number" name="notas[]" min="1" max="10">&nbsp;&nbsp;
+                    Nota 2 &nbsp;&nbsp;
+                    <input type="number" name="notas[]" min="1" max="10">&nbsp;&nbsp;
+                    Nota 3 &nbsp;&nbsp;
+                    <input type="number" name="notas[]" min="1" max="10">&nbsp;&nbsp;
+                    Nota 4 &nbsp;&nbsp;
+                    <input type="number" name="notas[]" min="1" max="10">&nbsp;&nbsp;
+                    <input type="submit" value="Ok" name="show">
                 </td>
             </tr>
             </form>
-        </table><br><br>
-        <?php
-            if (isset($_POST['save'])) {
-                $evaluaciones = $_POST['valores'];
-                $id= $_POST['cmbEstudiante'];
-                $notas = $_POST['notas'];
-                $idtipo = $_POST['cmbTipo'];
-                echo "<table>
-                <tr>
-                <th> Estudiante </th>
-                <th> Tipo de Evaluaion </th>";
-                for ($o=0; $o < $evaluaciones ; $o++) { 
-                    echo "<th>Nota " . ($o+1) . "</th>";
+        </table><br><br>';
+        
+        if (isset($_POST['show'])) {
+            echo "<form method='post'>"; 
+           $evaluacion = $_POST['cmbTipo'];
+           $alumno =$_POST['cmbEstudiante'];
+           $materia = $_POST['cmbMateria'];
+           $anio = $_POST['cmbAnio'];
+           $notas = $_POST['notas'];
+            $ce = new ControladorEst();
+            $ct = new ControladorTipo();
+           $alum = $ce->buscarxId($alumno);
+           $eva = $ct->ObtenerTipoxid($evaluacion);
+           echo "<table>
+                    <tr>
+                        <th> Alumno </th>
+                        <th> Evaluación </th>
+                        <th> Nota 1 </th>
+                        <th> Nota 2</th>
+                        <th> Nota 3 </th>
+                        <th> Nota 4 </th>
+                    </tr>
+                    <tr>
+                        <td>" .$alum[0]->getNombre()."&nbsp;".$alum[0]->getApellidos() ."</td>
+                        <td>" .$eva[0]->getTipo() . "</td>";
+                        foreach ($notas as $ke => $val) {
+                            echo "<td>" . $val. "</td>";
+                        }
+                echo "</tr></table><br><br>";  
+                echo "¿Son los datos correctos?<br><br>";
+               
+                echo "<input type='submit' value='Si' name='yes'>&nbsp;&nbsp;
+                        <input type='submit' value='No' name='No'>";
+                foreach ($notas as $ke => $val) {
+                    echo "<input type='hidden' name='tas[]' value='".$val."'>";    
                 }
-                echo "</tr>";
-                echo "<tr>";
-                $estudiantes = $ce->buscarxId($id);
-                echo "<td>" . $estudiantes[0]->getNombre() . "&nbsp;" . $estudiantes[0]->getApellidos() . "</td>";
-                $nombre = $ct->ObtenerTipoxid($idtipo);
-                echo "<td>" . $nombre[0]->getTipo() . "</td>";
-                for ($u=0; $u < sizeof($notas); $u++) { 
-                            echo "<td>";
-                            echo $notas[$u];
-                            echo "</td>";
-                }
-                echo "</tr></table><br><br>";
-                echo "¿Son los datos correctos?<br>";
-                echo '<form method="post"><br>
-                    <input type="submit" value="Si" name="si">&nbsp;
-                    <input type="submit" value="No" name="no"><br>
-                    </form>';
-            }
-            if (isset($_POST['si'])) {
-                include_once("../controlador/controladorNota.php");
-                $cn = new ControladorNota();
-                for ($n=0; $n < sizeof($notas); $n++) {
-                    $nota = new Nota();
-                    $nota->setIdNota(null);
-                    $nota->setIdEstudiante($estudiantes[0]->getIdEstudiante());
-                    $nota->setIdMateria($materia[0]->getIdMateria());
-                    $nota->setIdTipo($tipos[0]->getIdTipo());
-                    $nota->setNota($notas[$n]);
-                    $nota->setPeriodo("Periodo" . $n+1);
-                    $nota->setAnio($_POST['cmbAnio']);
-                    $nota->setPorcentaje(0.25);
-                    $cn ->agregarNota($nota);
-                }
-                // echo "<script type='text/javascript' languaje='javascript'>
-                //     alert('Registro agregado con exito!');
-                //     window.location='agregarNota.php';
-                //     </script>";
-            }
+                echo "<input type='hidden' name='anio' value='" . $anio . "'>";
+                echo "</form>";      
+        }if (isset($_POST['yes'])) {
+            include_once("../controlador/controladorNota.php");
+            $calificaciones=$_POST['tas'];
+            $year =$_POST['anio'];
+            $cn = new ControladorNota();
+            for ($i=0; $i < sizeof($calificaciones); $i++) { 
+                $nota = new Nota();
+                $nota->setIdNota(null);
+                $nota->setIdEstudiante($estudiantes[0]->getIdEstudiante());
+                $nota->setIdMateria($materia[0]->getIdMateria());
+                $nota->setIdTipo($tipos[0]->getIdTipo());
+                $nota->setNota($calificaciones[$i]);
+                $nota->setPeriodo("Periodo" . ($i+1));
+                $nota->setAnio($year);
+                $nota->setPorcentaje(0.25);
+                $cn ->agregarNota($nota);
+            } 
+            echo "<script type='text/javascript' laguaje='javascript'>
+                    alert('Registro guardado con Exito!');
+                    window.location='./agregarNota.php';
+                </script>";  
+         }elseif (isset($_POST['no'])) {
+            echo "<script type='text/javascript' languaje='javascript'>
+                    alert('Los datos han sido borrados, presione aceptar para volver a editar');
+                    window.location='./agregarNota.php';
+                </script>";
+         }
+        
         ?>
     </article>
 </body>
